bbc48691889bf64a0af2490e8d55f7545df7a072
Juan-Alvarado <45235018+Juan-Alvarado@users.noreply.github.com>
2019-01-27 18:46:07 +0000

    Update index.js

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -4,15 +4,16 @@ import { h, render, Component } from 'preact';
 export default class Button extends Component {
 
 	// rendering a function when the button is clicked
-	render({clickFunction}) {
-		if(!clickFunction){
-			clickFunction = () => {
+	render() {
+		let cFunction = this.props.clickFunction;
+		if(typeof cFunction !== 'function'){
+			cFunction = () => {
 				console.log("passed something as 'clickFunction' that wasn't a function !");
 			}
-		}	
+		}
 		return (
 			<div>
-				<button onClick={clickFunction}>
+				<button onClick={cFunction}>
 					Display Weather
 				</button>
 			</div>
